8e9d6634a1a9d1d3364fa39e6c205645cb77a460
Karter <kdlmuzixnerd14@gmail.com>
2022-06-30 10:27:47 -0700

    commit tree d32ef91044ef73150b723cceec1557eac4888de6

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+
+function findMatching(names, arg)
+{
+    //take array & string, returns array of matches (make it not case sensitive)
+    const list = names.filter(function(word)
+    {
+        if(word.toLowerCase() === arg.toLowerCase())
+        {return word;}  
+    });
+    return list;
+}
+
+function fuzzyMatch(names, arg)
+{
+    //take array & string, returns array of names that begin with provided arg
+    const list = names.filter(function(word)
+    {
+        if(word.startsWith(arg))
+        {
+            return word;
+        }  
+    });
+    return list;
+}
+
+function matchName(names, arg)
+{
+    //take array & string, return element whose name matches string
+    const list = names.filter(function(word)
+    {
+        if(word.name === arg)
+        {
+            return word;
+        }  
+    });
+    return list;
+}
